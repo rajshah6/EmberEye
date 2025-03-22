d549ab3fd0406bb96264c1949b98d3c873ae4781
Aumkar Mali <78087877+AumkarMali@users.noreply.github.com>
2025-03-22 10:51:25 -0400

    Update App.jsx

--- GenAiGenesis-web/src/App.jsx
+++ GenAiGenesis-web/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/login';
 import SignUp from './components/newuser';
 import IntroductionPage from './components/introductionPage';
-import Enter from './components/enter';
+
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/ent" element={<Enter />} />
+   
         <Route path="/main" element={<IntroductionPage />} />
       </Routes>
     </Router>
