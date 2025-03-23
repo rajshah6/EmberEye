f50cc0088dc530cea7e650f57c47740d8ed057cf
Aumkar Mali <78087877+AumkarMali@users.noreply.github.com>
2025-03-23 10:23:41 -0400

    Update vite.config.js

--- Map-main/vite.config.js
+++ Map-main/vite.config.js
@@ -1,12 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'], // Replace with the packages you want to exclude
-    },
-  },
-});
+};
